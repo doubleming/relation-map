20fd540253032b9b997c41f8f5758997ba76330d
zhoumingming <double_ming@outlook.com>
2023-07-28 08:37:26 +0800

    增加演示文档操作说明

--- play/src/main.ts
+++ play/src/main.ts
@@ -3,6 +3,7 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+  <div style="margin-bottom: 10px">操作提示：鼠标中间拖动画布、点击节点拖拽节点、ctrl+滚轮缩放、 点击节点则以该节点为中心，动画转移画布</div>
   <canvas id="canvas"></canvas>
   </div>
 `
